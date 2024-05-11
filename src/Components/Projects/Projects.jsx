77799602c710e2cfce8f00ef34e4df2525e880aa
Ahmed Hassan <ah2300487@gmail.com>
2024-05-11 10:50:38 +0200

    remove some projects

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -4,9 +4,6 @@ import noxe from "../../assets/noxe.png"
 import backery from "../../assets/Backery.png"
 import fokir from "../../assets/Fokir.png"
 import simone from "../../assets/simone.png"
-import xo from "../../assets/xo.png"
-import crud from "../../assets/cruds.png"
-import calculator from "../../assets/calculator.png"
 import AProfile from "../../assets/A..png"
 import { Helmet } from 'react-helmet'
 import educavo from '../../assets/educavo.png'
@@ -45,9 +42,7 @@ export default function Project() {
                     <li className="nav-item">
                         <span style={darkMode == false ? { backgroundColor: 'white', color: 'black' } : { backgroundColor: 'black', color: 'white' }} onClick={() => { setMode("landingPage") }} className={`nav-link ${style.tabItem} ${mode == 'landingPage' ? 'text-primary' : ""}`} >Landing Pages</span>
                     </li>
-                    <li className="nav-item">
-                        <span style={darkMode == false ? { backgroundColor: 'white', color: 'black' } : { backgroundColor: 'black', color: 'white' }} onClick={() => { setMode("vanilla") }} className={`nav-link ${style.tabItem} ${mode == 'vanilla' ? 'text-primary' : ""}`} >Vanilla JS</span>
-                    </li>
+                    
                 </ul>
                 <div className="row py-5 g-4">
                     {mode == 'all' || mode == 'landingPage' ?
@@ -187,42 +182,6 @@ export default function Project() {
                                     <i class={`fa-solid fa-code fs-5 mx-1  ${style.bgStyle} rounded-circle p-3 text-white`}></i>
                                 </a>
                                 <a target='_blank' href="https://ahmedhassan222.github.io/noxe/">
-                                    <i class={`fa-solid fa-display fs-5 mx-1  ${style.bgStyle} rounded-circle p-3 text-white`}></i>
-                                </a>
-                            </div>
-                        </div> : ""}
-                    {mode == 'all' || mode == 'vanilla' ?
-                        <div className={`col-sm-12 col-md-4 col-lg-4 position-relative ${style.box} `} >
-                            <img className={`w-100 ${style.imgStyle}`} src={xo} alt="" />
-                            <div className={` ${style.positionStyle} position-absolute d-flex justify-content-center align-items-center`}>
-                                <a target='_blank' href="https://github.com/AhmedHassan222/X-O_Game">
-                                    <i class={`fa-solid fa-code fs-5 mx-1  ${style.bgStyle} rounded-circle p-3 text-white`}></i>
-                                </a>
-                                <a target='_blank' href="https://ahmedhassan222.github.io/X-O_Game/">
-                                    <i class={`fa-solid fa-display fs-5 mx-1  ${style.bgStyle} rounded-circle p-3 text-white`}></i>
-                                </a>
-                            </div>
-                        </div> : ""}
-                    {mode == 'all' || mode == 'vanilla' ?
-                        <div className={`col-sm-12 col-md-4 col-lg-4 position-relative ${style.box} `} >
-                            <img className={`w-100 ${style.imgStyle}`} src={calculator} alt="" />
-                            <div className={` ${style.positionStyle} position-absolute d-flex justify-content-center align-items-center`}>
-                                <a target='_blank' href="https://github.com/AhmedHassan222/Calculator">
-                                    <i class={`fa-solid fa-code fs-5 mx-1  ${style.bgStyle} rounded-circle p-3 text-white`}></i>
-                                </a>
-                                <a target='_blank' href="https://ahmedhassan222.github.io/Calculator/">
-                                    <i class={`fa-solid fa-display fs-5 mx-1  ${style.bgStyle} rounded-circle p-3 text-white`}></i>
-                                </a>
-                            </div>
-                        </div> : ""}
-                    {mode == 'all' || mode == 'vanilla' ?
-                        <div className={`col-sm-12 col-md-4 col-lg-4 position-relative ${style.box}  `} >
-                            <img className={`w-100 ${style.imgStyle}`} src={crud} alt="" />
-                            <div className={` ${style.positionStyle} position-absolute d-flex justify-content-center align-items-center`}>
-                                <a target='_blank' href="https://github.com/AhmedHassan222/CRUD-system">
-                                    <i class={`fa-solid fa-code fs-5 mx-1  ${style.bgStyle} rounded-circle p-3 text-white`}></i>
-                                </a>
-                                <a target='_blank' href="https://ahmedhassan222.github.io/CRUD-System/">
                                     <i class={`fa-solid fa-display fs-5 mx-1  ${style.bgStyle} rounded-circle p-3 text-white`}></i>
                                 </a>
                             </div>
